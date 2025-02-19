a8ab0990bfbd52407d4a5e78024d1b6ee01255ca
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2025-02-18 23:06:03 -0300

    chore: add factory-implementation in typescript

---
+++ factory-implementation.ts
@@ -0,0 +1,77 @@
+// common interface
+interface InterativeToy{
+  interact():void;
+}
+
+// concrete implementations
+class Barbie implements InterativeToy{
+  public interact(){
+    console.log('You can be what you want!')
+  }
+}
+
+class HotWheels implements InterativeToy{
+  public interact(){
+    console.log('Turbo Mode!');
+  }
+}
+
+class GenericToy implements InterativeToy{
+  public interact(){
+    console.log('Smile - Butterfly ♪♪');
+  }
+}
+
+// factory interface
+interface FactoryInterativeToy{
+  create(): InterativeToy;
+}
+
+// factory implementations
+class BarbieFactory implements FactoryInterativeToy{
+  public create(){
+    return new Barbie();
+  }
+}
+
+class HotWheelsFactory implements FactoryInterativeToy{
+  public create(){
+    return new HotWheels();
+  }
+}
+
+class GenericToyFactory implements FactoryInterativeToy{
+  public create(){
+    return new GenericToy();
+  }
+}
+
+
+// use case
+
+enum Preference{
+  DOLLS,
+  CARS,
+  OTHER
+}
+
+function makeAToy(preference: Preference = Preference.OTHER): InterativeToy{
+  const FACTORIES: Record<Preference, FactoryInterativeToy> = {
+    [Preference.DOLLS]: new BarbieFactory(),
+    [Preference.CARS]: new HotWheelsFactory(),
+    [Preference.OTHER]: new GenericToyFactory(),
+  };
+
+  const matchFactory = FACTORIES[preference];
+
+  return matchFactory.create();
+}
+
+// view console
+makeAToy(Preference.OTHER).interact();
+makeAToy(Preference.CARS).interact();
+makeAToy(Preference.DOLLS).interact();
+
+//defaults to OTHER preference
+const toy = makeAToy();
+toy.interact();
